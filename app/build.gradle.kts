6a78f6301d2864ece6e2e4f4d614fdd706215f03
st412m <staroverm@gmail.com>
2025-03-18 21:39:49 +0300

    homework14

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -35,7 +35,8 @@ android {
         versionName = "0.1.2" // X.Y.Z; X = Major, Y = minor, Z = Patch level
 
         // Custom test runner to set up Hilt dependency graph
-        testInstrumentationRunner = "com.google.samples.apps.nowinandroid.core.testing.NiaTestRunner"
+//        testInstrumentationRunner = "com.google.samples.apps.nowinandroid.core.testing.NiaTestRunner" заменили на каспрессо
+        testInstrumentationRunner = "com.kaspersky.kaspresso.runner.KaspressoRunner"
     }
 
     buildTypes {
@@ -126,12 +127,19 @@ dependencies {
     androidTestImplementation(projects.core.testing)
     androidTestImplementation(projects.core.dataTest)
     androidTestImplementation(projects.core.datastoreTest)
-    androidTestImplementation(libs.androidx.test.espresso.core)
+//    androidTestImplementation(libs.androidx.test.espresso.core) могут быть проблемы с версиями
     androidTestImplementation(libs.androidx.compose.ui.test)
     androidTestImplementation(libs.hilt.android.testing)
     androidTestImplementation(libs.kotlin.test)
 
     baselineProfile(projects.benchmarks)
+    implementation ("com.google.android.material:material:1.10.0")
+    androidTestImplementation("com.kaspersky.android-components:kaspresso-compose-support:1.6.0") {
+        exclude(module = "protobuf-lite")
+    }
+    androidTestImplementation("com.kaspersky.android-components:kaspresso:1.6.0") {
+        exclude(module = "protobuf-lite")
+    }
 }
 
 baselineProfile {
