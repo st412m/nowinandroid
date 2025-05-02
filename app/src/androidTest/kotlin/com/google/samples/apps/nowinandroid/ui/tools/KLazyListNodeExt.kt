b312a128cec6a6001c009855dc2e628f1f49cc91
st412m <staroverm@gmail.com>
2025-05-02 19:12:57 +0300

    homework16

--- app/src/androidTest/kotlin/com/google/samples/apps/nowinandroid/ui/tools/KLazyListNodeExt.kt
+++ app/src/androidTest/kotlin/com/google/samples/apps/nowinandroid/ui/tools/KLazyListNodeExt.kt
@@ -16,9 +16,16 @@
 
 package com.google.samples.apps.nowinandroid.ui.tools
 
+import android.content.res.Resources.NotFoundException
+import androidx.compose.ui.semantics.getOrNull
 import androidx.compose.ui.test.ExperimentalTestApi
+import com.google.samples.apps.nowinandroid.core.designsystem.LazyListLengthSemantics
+import io.github.kakaocup.compose.node.action.NodeActions
 import io.github.kakaocup.compose.node.element.lazylist.KLazyListItemNode
 import io.github.kakaocup.compose.node.element.lazylist.KLazyListNode
+import io.github.kakaocup.compose.node.element.list.KListItemNode
+import io.github.kakaocup.compose.node.element.list.KListNode
+import kotlin.math.min
 
 @OptIn(ExperimentalTestApi::class)
 inline fun <reified T : KLazyListItemNode<*>> KLazyListNode.invokeAtIndex(
@@ -30,4 +37,38 @@ inline fun <reified T : KLazyListItemNode<*>> KLazyListNode.invokeAtIndex(
         setName(lazyList.getName().withParent("$targetIndex"))
         function()
     }
+}
+
+inline fun <reified T : KListItemNode<*>> KListNode.invokeByPredicate(
+    targetIndex: Int,
+    blockName: String,
+    limiter: Int,
+    predicate: T.() -> Boolean,
+    function: T.() -> Unit,
+) {
+    val list = this
+    var findBlockCounter = 0
+    val max = min(limiter, getSize())
+    for (i in 0 until max) {
+        childAt<T>(i) {
+            if (predicate()) {
+                if (findBlockCounter == targetIndex) {
+                    setName(list.getName().withParent("$targetIndex's block of $blockName"))
+                    function()
+                    return
+                }
+                findBlockCounter++
+            }
+        }
+    }
+    throw NotFoundException("Not found block with $targetIndex index of $blockName")
+}
+
+fun NodeActions.getSize(): Int {
+    return this.delegate
+        .interaction
+        .semanticsNodeInteraction
+        .fetchSemanticsNode()
+        .config
+        .getOrNull(LazyListLengthSemantics) ?: 0
 }
